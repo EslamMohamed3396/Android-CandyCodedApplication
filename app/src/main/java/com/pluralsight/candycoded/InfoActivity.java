9243d9f00baa0d26cc436752324e2917e60e2eba
EslamMohamed3396 <em0109224@gmail.com>
2019-11-03 02:12:32 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,7 +36,14 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Uri uriPhone = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uriPhone);
+        startActivity(phoneIntent);
+    }
 }
